9473d752abd9f72fb75a59efa04f2bb7f66bcf74
Jose Andres Mena <andres.menaarias@gmail.com>
2021-04-13 17:59:52 -0600

    Ejemplo 1 de Pthreads modificado en clase 2

--- Ejemplos de Clase/01_Pthreads/hello.c
+++ Ejemplos de Clase/01_Pthreads/hello.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-void* helloWorldRun(void* data) {
-
-    printf("Hello world from secondary thread!!!\n");
-    printf("data = %zu\n", (size_t)data);
-
-    return NULL;
-}
+void* helloWorldRun(void* data);
 
 int main(void) {
-
-    printf("Hello World from main thread\n");
     pthread_t thread;
-    pthread_create(&thread,  NULL, helloWorldRun, (void*)2021);
+    if (pthread_create(&thread,  NULL, helloWorldRun, (void*)2021) == EXIT_SUCCESS) {
+        printf("Hello World from main thread\n");
+        pthread_join(thread, NULL);
+        return EXIT_SUCCESS;
+    } else {
+        fprintf(stderr, "Could not create secondary thread");
+        return EXIT_FAILURE;
+    }
+}
 
-    pthread_join(thread, NULL);
-
-    return 0;
+void* helloWorldRun(void* data) {
+    printf("Hello world from secondary thread!!!\n");
+    printf("data = %zu\n", (size_t)data);
+    return NULL;
 }
